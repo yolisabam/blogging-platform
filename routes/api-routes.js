55dd716d577acbdbec48a865f91128eb33e1d884
Yolisa Bam <yolisabam@Yolisas-MacBook-Pro.local>
2017-12-20 18:22:43 -0800

    starting to add queries for routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,9 +2,21 @@ const db       = require("../models");
 const express  = require("express");     
 const passport = require("passport"); 
 
-
 const NewsAPI = require("newsapi");
 const newsapi = new NewsAPI("3e41864bbb6d4719a2cebfdf71e27156");
 const cron = require("node-cron");
 
 const router = express.Router();
+
+// views //
+
+// display all categories on homepage
+router.get("/", function(request, response) {
+  db.Category.all().then(function(data) {
+    const categories = [];
+    for (let instance in data) {
+      categories.push(data[instance].dataValues);
+    }
+
+  })
+})
